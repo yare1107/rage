233044bd45309c12701ea17176a36943af022691
Yare <sant0711yt@gmail.com>
2025-04-28 16:09:15 -0500

    Add files via upload

---
+++ plugins/ff-guerradeclanes.js
@@ -0,0 +1,107 @@
+let handler = async (m, { isPrems, conn }) => {
+let time = global.db.data.users[m.sender].lastcofre + 0 // 36000000 10 Horas //86400000 24 Horas
+if (new Date - global.db.data.users[m.sender].lastcofre < 0) throw `[❗𝐈𝐍𝐅𝐎❗] 𝚈𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚂𝚃𝙴 𝚃𝚄 𝙲𝙾𝙵𝚁𝙴\𝚗𝚅𝚄𝙴𝙻𝚅𝙴 𝙴𝙽 *${msToTime(time - new Date())}* 𝙿𝙰𝚁𝙰 𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚁`
+
+let img = 'https://qu.ax/msPxh.jpg' 
+let texto = `
+       𝙇𝙞𝙨𝙩𝙖 𝙂𝙪𝙚𝙧𝙧𝙖 𝙙𝙚 𝘾𝙡𝙖𝙣𝙚𝙨
+
+ ¬ 𝐉𝐔𝐆𝐀𝐃𝐎𝐑𝐄𝐒 𝐏𝐑𝐄𝐒𝐄𝐍𝐓𝐄𝐒
+
+    
+          𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 1
+    
+    👑 ┇ 
+    🥷🏻 ┇  
+    🥷🏻 ┇ 
+    🥷🏻 ┇ 
+          
+         𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 2
+    
+    👑 ┇ 
+    🥷🏻 ┇ 
+    🥷🏻 ┇ 
+    🥷🏻 ┇ 
+
+         𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 3
+    
+    👑 ┇ 
+    🥷🏻 ┇ 
+    🥷🏻 ┇ 
+    🥷🏻 ┇ 
+
+         𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 4
+    
+    👑 ┇ 
+    🥷🏻 ┇ 
+    🥷🏻 ┇ 
+    🥷🏻 ┇ 
+
+         𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 5
+    
+    👑 ┇ 
+    🥷🏻 ┇ 
+    🥷🏻 ┇ 
+    🥷🏻 ┇ 
+
+         𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 6
+    
+    👑 ┇ 
+    🥷🏻 ┇ 
+    🥷🏻 ┇ 
+    🥷🏻 ┇  
+
+	     𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 7
+    
+    👑 ┇ 
+    🥷🏻 ┇ 
+    🥷🏻 ┇ 
+    🥷🏻 ┇  
+
+	     𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 8
+    
+    👑 ┇ 
+    🥷🏻 ┇ 
+    🥷🏻 ┇ 
+    🥷🏻 ┇  
+
+	     𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 9
+    
+    👑 ┇ 
+    🥷🏻 ┇ 
+    🥷🏻 ┇ 
+    🥷🏻 ┇  
+
+         𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 10
+    
+    👑 ┇ 
+    🥷🏻 ┇ 
+    🥷🏻 ┇ 
+    🥷🏻 ┇  
+
+	`
+
+const fkontak = {
+	"key": {
+    "participants":"0@s.whatsapp.net",
+		"remoteJid": "status@broadcast",
+		"fromMe": false,
+		"id": "Halo"
+	},
+	"message": {
+		"contactMessage": {
+			"vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+		}
+	},
+	"participant": "0@s.whatsapp.net"
+}
+await conn.sendFile(m.chat, img, 'hades.jpg', texto, fkontak)
+global.db.data.users[m.sender].lastcofre = new Date * 1
+}
+handler.help = ['guerradeclanes']
+handler.tags = ['freefire']
+handler.command = ['listagdc'] 
+handler.register = true
+handler.admin = true
+export default handler
+
